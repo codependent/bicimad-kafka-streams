9c0fa30e7f0e39799362b0602d0c9cc8322ceae3
jose <jainigo@profile.es>
2018-08-29 12:02:42 +0200

    BicimadWebClientTest disabled

--- src/test/kotlin/com/codependent/bicimad/webclient/BiciMacWebClientTest.kt
+++ src/test/kotlin/com/codependent/bicimad/webclient/BiciMacWebClientTest.kt
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CountDownLatch
 
+@Disabled
 class BiciMacWebClientTest {
-
 
     private val biciMadWebClient = BiciMadWebClient(
             "https://rbdata.emtmadrid.es:8443/BiciMad",
